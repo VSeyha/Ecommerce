bfe0c7417f7df0320170c4d07e5a9def90d4ddfd
seyha <89523604+VSeyha@users.noreply.github.com>
2023-02-19 21:34:23 +0700

    update

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -1,8 +1,13 @@
+// Import the mongoose module
 const mongoose = require("mongoose");
-mongoose.connect(
-    process.env.MONGODB_URI, 
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-);
+
+mongoose.set('strictQuery', false);
+
+// Define the database URL to connect to.
+const mongoDB = "mongodb://127.0.0.1/ecommerce";
+
+// Wait for database to connect, logging an error if there is a problem 
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
